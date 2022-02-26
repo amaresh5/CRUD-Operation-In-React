103e368135228c55f3cdbf2f68a82a14944abe6c
amaresh5 <amareshmali1999@gmail.com>
2022-02-26 12:34:08 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from "./components/Navbar";
+import Aside from "./components/Aside";
+
+function App() {
+  return (
+   <>
+<Navbar/>
+<Aside/>
+  </>
+
+  );
+}
+
+export default App;
